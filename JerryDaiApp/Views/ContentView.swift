7cd2e16ac203a099f15281f33c28cc2fe73d25ef
lcs-jdai <jdai@lcs.on.ca>
2021-11-29 17:17:09 -0500

    Finished the login and sign page. I used text field and buttons, still need to keep working on the views that links to these buttons.

--- JerryDaiApp/Views/ContentView.swift
+++ JerryDaiApp/Views/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("bg")
+                .resizable()
+                .frame(width: 400, height: 600, alignment: .center)
+            
+            Image ("icon")
+                .resizable()
+                .frame(width: 80, height: 80, alignment: .center)
+                .padding(.bottom, 400)
+        
+            
+            VStack{
+            TextField(" Username", text: self.$email)
+                .frame(width: 250, height: 40, alignment: .center)
+                           .textFieldStyle(RoundedBorderTextFieldStyle())
+                           .cornerRadius(6)
+                           .padding([.leading, .trailing], 24)
+            
+            SecureField(" Password", text: self.$password)
+                .frame(width: 250, height: 40, alignment: .center)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .cornerRadius(6)
+                            .padding([.leading, .trailing], 24)
+                
+                Button("Login") {
+                     print("Button was pressed")
+                }
+                
+                .frame(width: 250, height: 40, alignment: .center)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(6)
+                .padding(.top,30)
+
+                HStack {
+                    Text ("Forget your password?")
+                        .font(.system(size: 15))
+                        .padding(.top,30)
+                        .foregroundColor(Color.white)
+                    
+                    Button("Sign up") {
+                         print("Button was pressed")
+                    }
+                    
+                    .frame(width:65, height: 25, alignment: .center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(6)
+                    .padding(.top,30)
+                    .font(.system(size: 15))
+                }
+                
+                
+    
+            }
+        }
     }
 }
 
