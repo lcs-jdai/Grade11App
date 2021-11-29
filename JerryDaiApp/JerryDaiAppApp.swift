f163065a5e0a91dc1210beccd824e1bde5704f7b
lcs-jdai <jdai@lcs.on.ca>
2021-11-29 14:26:06 -0500

    Initial Commit

---
+++ JerryDaiApp/JerryDaiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JerryDaiAppApp.swift
+//  JerryDaiApp
+//
+//  Created by Jerry Dai on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct JerryDaiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
